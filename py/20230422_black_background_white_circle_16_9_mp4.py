06a137be0a01ccee7b074f24070cd9cc67d7068c
g1041021 <129607461+g1041021@users.noreply.github.com>
2023-04-22 19:02:27 +0900

    Update 20230422_black_background_white_circle_16_9_mp4.py
    以下の修正をしました。
    ・円の大きさが一定になるようにしました。
    ・円の開始位置を修正しました。
    ・動画時間を10秒に変更しました。

--- py/20230422_black_background_white_circle_16_9_mp4.py
+++ py/20230422_black_background_white_circle_16_9_mp4.py
@@ -6,9 +6,9 @@ from moviepy.video.io.ffmpeg_writer import FFMPEG_VideoWriter
 
 def make_frame(t):
     # 動画の長さをt秒として、白い円が画面内を移動する位置を算出する
-    x = int(1920 * (1 - t / 5))
+    x = int(1920 * (1 - t / 5) + 200)
     y = 540
-    r = int(200 * (1 - t / 5))
+    r = int(200)
 
     # 黒い背景の画像を作成する
     img = np.zeros((1080, 1920, 3), dtype=np.uint8)
@@ -20,5 +20,5 @@ def make_frame(t):
 
 
 # 動画ファイルを生成する
-clip = VideoClip(make_frame, duration=5)
+clip = VideoClip(make_frame, duration=10)
 clip.write_videofile("output.mp4", fps=30, codec="mpeg4")

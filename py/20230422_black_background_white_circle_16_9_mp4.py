30cdfdc5500ff754616c888e16b82df0a09cd085
g1041021 <129607461+g1041021@users.noreply.github.com>
2023-04-22 18:55:01 +0900

    Create 20230422_black_background_white_circle_16_9_mp4.py
    ChatGPTに「pythonで、黒い背景で、白い円が画面右端から侵入してきて、画面左端から出ていく、16:9のmp4動画ファイルを生成する方法を教えてください。」と依頼して、生成してもらいました。

---
+++ py/20230422_black_background_white_circle_16_9_mp4.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+from moviepy.editor import VideoClip
+from moviepy.video.io.ffmpeg_writer import FFMPEG_VideoWriter
+
+
+def make_frame(t):
+    # 動画の長さをt秒として、白い円が画面内を移動する位置を算出する
+    x = int(1920 * (1 - t / 5))
+    y = 540
+    r = int(200 * (1 - t / 5))
+
+    # 黒い背景の画像を作成する
+    img = np.zeros((1080, 1920, 3), dtype=np.uint8)
+
+    # 白い円を描画する
+    cv2.circle(img, (x, y), r, (255, 255, 255), -1)
+
+    return img
+
+
+# 動画ファイルを生成する
+clip = VideoClip(make_frame, duration=5)
+clip.write_videofile("output.mp4", fps=30, codec="mpeg4")
